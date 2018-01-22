cda45ae69c490af6b9f5fa9f919acfa070c8c99f
ZDT@VAIO <dev@dthappy.com>
2018-01-22 19:01:55 +0800

    update config

--- wx/server/config.js
+++ wx/server/config.js
@@ -3,7 +3,7 @@ const CONF = {
     rootPathname: '',
 
     // 微信小程序 App ID
-    appId: 'wxbfef678622aa5a51',
+    appId: 'appid',
 
     // 微信小程序 App Secret
     appSecret: '',
@@ -21,7 +21,7 @@ const CONF = {
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        pass: 'wxbfef678622aa5a51',
+        pass: 'appid',
         char: 'utf8mb4'
     },
 
