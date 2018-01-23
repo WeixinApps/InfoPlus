5935f9ad8a2fb685e7faaceabb16ae632153eaa0
ZDT@VAIO <dev@dthappy.com>
2018-01-24 00:25:28 +0800

    init addTask page

--- wx/server/controllers/demo.js
+++ wx/server/controllers/demo.js
@@ -1,7 +1,9 @@
 const demo = {
     async get(ctx, next){
+        console.log(ctx.state);
         ctx.state.data = {
-            msg:'Hello World2'
+            msg:'Hello World2',
+            info:'H'
         }
     },
     async post(ctx, next){
